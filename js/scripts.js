3900c76e8b7afd04914cd691ed1482b3b90873b4
Lieghti <lieghti@gmail.com>
2017-11-05 15:30:20 -0800

    check to see if script function divisisble by 5 is working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // business logic
 var PingPong = function(number) {
 
- if ((number % 3 === 0)) { - this one works
+ //if ((number % 3 === 0)) { - this one works
 
-// if (number % 5 === 0) { -- this one works too
+if (number % 5 === 0) { -- this one works too
 
 // if ((number % 3 === 0) && (number % 5 === 0)) {
   //   return true;

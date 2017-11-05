48c90bd8edd18da555364d5ecc60a85deb00e5f6
Lieghti <lieghti@gmail.com>
2017-11-05 15:27:18 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// business logic
+var PingPong = function(number) {
+
+ // if ((number % 3 === 0)) { - this one works
+
+// if (number % 5 === 0) { -- this one works too
+
+if ((number % 3 === 0) && (number % 5 === 0)) {
+  //   return true;
+  // } else {
+    return true;
+ } else {
+    return false;
+  }
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    var result = PingPong(number);
+    $("#result").text(result);
+
+  //   $(".number").text(number);
+  //
+  //   if (!result) {                 // same as writing if (result === false)
+  //     $(".ping").text("not");
+  //   } else {
+  //     $(".pong").text("");
+  //   }
+  //
+  //   $("#result").show();
+   });
+});

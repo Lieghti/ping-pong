6bcd9ba2aeb8a9714e8ec313b86b6f328b3657fd
Guest User <Guest@Epicodus-9D.local>
2017-11-06 20:14:56 -0800

    moved some things around in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
 // business logic
-var PingPong = function(number) {
-  if ((number % 3 === 0)) {
-    return true;
-    } else {
-    return false;
-     }
 
-// if (number % 5 === 0) {
-//
-// if ((number % 3 === 0) && (number % 5 === 0)) {
-//     return true;
-//   } else {
-//     return true;
-//  } else {
-//     return false;
-//   }
+var PingPong = function(number) {
+  var emptyArray =[];
+  for (i = 1; number < 15; i +=1 ) {
+    if (number % 3 === 0) && (number % 5 === 0){
+      return emptyArray.push("ping pong");
+    } else if (number % 5 === 0) {
+      return emptyArray.push("pong");
+    } else if (number % 3 === 0) {
+      return emptyArray.push ("ping")
+    }
+
+
+
+
+  // if (number % 3 === 0) {
+  //   return emptyArray.push("ping pong");
+  // } else if ((number % 5 === 0)){
+  //   return span.pong;
+  // } else if ((number % 3 === 0) && (number % 5 === 0)){
+  //   return span.both;
+  // }
+
+ //     if (number % 5 === 0) {
+ //
+ //  if ((number % 3 === 0) && (number % 5 === 0)) {
+ //    return true;
+ //  } else {
+ //    return true;
+ // } else {
+ //    return false;
+ //  }
 
 };
 
@@ -23,7 +39,7 @@ $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
-    var result = PingPong(number);
+    //var result = PingPong(number);
     $("#result").text(result);
 
    $(".number").text(number);

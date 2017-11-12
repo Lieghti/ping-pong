8f64ab4a293707a57ee2eb1bcc235ba4cbcc42c9
Lieghti <lieghti@gmail.com>
2017-11-11 20:29:46 -0800

    remove old code that no longer works

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,7 @@
 var PingPong = function(number) {
   var numberList =[];
   for (i = 1; i <= number; i++ ) {
-    if ((number % 3 === 0) && (number % 5 === 0)){
-      return emptyArray.push("ping pong");
-    } else if (number % 5 === 0) {
-      return emptyArray.push("pong");
-    } else if (number % 3 === 0) {
-      return emptyArray.push ("ping")
-    } else {
-      return number;
-    }
-    var displayNumber = document.createElement("li").lastChild;
+  
 
 
 };

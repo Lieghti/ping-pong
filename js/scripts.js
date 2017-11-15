62913ff0d1cb3240d6301a58cf41d526913a0ae4
Lieghti <lieghti@gmail.com>
2017-11-15 10:19:50 -0800

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,34 @@
-// business logic
-
-var PingPong = function(number) {
+var PingPong = function (number) {
   var numberList =[];
-  debugger;
+    return numberList;
+};
+
   for (i = 1; i <= number; i++ ) {
-    if (i % 3 === 0) {
-      numberList.push("Ping");
-    } else if (i % 5 === 0){
-      numberList.push("pong");
-    } else if (i % 15 === 0) {
+    if (i % 15 === 0) {
       numberList.push("pingpong");
+    } else if (i % 5 === 0){
+      numberList.push ("pong");
+    } else if (i % 3 === 0) {
+      numberList.push ("Ping");
     } else {
-      numberList.push(" " + i);
+      numberList.push ("" + i);
     }
+}
 
 
-  };
 
-  // user interface logic
+  // user  logic
   $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
       event.preventDefault();
+
       var number = parseInt($("input#number").val());
 
       var result = PingPong(number);
-
-      $("#result").text(result);
-      $("#result").show();
+      var lineItem = result.forEach(function(PingPongInput){
+        $("ul#result").append("<li>" + pingPongInput + "</li>");
+      });
+      $("ul#result").show();
     });
 
   });
